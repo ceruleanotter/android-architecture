4e560d4e89cc8099df0c66c50e3b721bbcb740e7
Lyla McBeath Fujiwara <lmf@google.com>
2019-07-02 18:56:30 -0700

    Fix minor issue with checkbox animation

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragment.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragment.kt
@@ -25,6 +25,7 @@ import android.view.ViewGroup
 import android.widget.CheckBox
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.android.architecture.blueprints.todoapp.EventObserver
 import com.example.android.architecture.blueprints.todoapp.R
@@ -95,6 +96,16 @@ class TaskDetailFragment : Fragment() {
                 }
             }
         }
+
+        viewModel.completed.observe(this, Observer { isChecked ->
+            viewDataBinding.taskDetailComplete.apply {
+                this.isChecked = isChecked
+
+                // Makes it so that the checkbox doesn't animate in when checked
+                jumpDrawablesToCurrentState()
+            }
+        })
+
         viewDataBinding.setLifecycleOwner(this.viewLifecycleOwner)
         setHasOptionsMenu(true)
         return view
