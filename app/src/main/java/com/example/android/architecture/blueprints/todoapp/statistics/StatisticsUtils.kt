5812875c0ccda4e6c32b0fc726028c64331c3560
Lyla McBeath Fujiwara <lmf@google.com>
2019-07-12 16:53:45 -0700

    Silly TDD example - no empty case

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtils.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtils.kt
@@ -22,15 +22,17 @@ import com.example.android.architecture.blueprints.todoapp.data.Task
  * Function that does some trivial computation. Used to showcase unit tests.
  */
 internal fun getActiveAndCompletedStats(tasks: List<Task>?): StatsResult {
-    // TODO implement task percentage complete calculation
-    val firstTask = tasks?.get(0)
 
-    if (firstTask != null && firstTask.isCompleted) {
-        return StatsResult(0f, 100f)
+    return if (tasks == null) {
+        StatsResult(0f, 0f)
     } else {
-        return StatsResult(100f, 0f)
+        val totalTasks = tasks.size
+        val numberOfActiveTasks = tasks.count { it.isActive }
+        StatsResult(
+            activeTasksPercent = 100f * numberOfActiveTasks / tasks.size,
+            completedTasksPercent = 100f * (totalTasks - numberOfActiveTasks) / tasks.size
+        )
     }
-
 }
 
 data class StatsResult(val activeTasksPercent: Float, val completedTasksPercent: Float)
