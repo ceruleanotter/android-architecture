9ea38c3a9a3816bd9dd176d47ac63fd78fa18184
Lyla Fujiwara <3414789+ceruleanotter@users.noreply.github.com>
2019-11-19 22:10:18 -0800

    TasksActivityTest without any idling resources editTask flakey

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/TasksActivityTest.kt
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.architecture.blueprints.todoapp
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import com.example.android.architecture.blueprints.todoapp.tasks.TasksActivity
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.core.IsNot.not
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Large End-to-End test for the tasks module.
+ */
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class TasksActivityTest {
+
+    private lateinit var repository: TasksRepository
+
+    @Before
+    fun init() {
+        repository = ServiceLocator.provideTasksRepository(getApplicationContext())
+        // TODO why is this run blocking instead of runBlockingTest? runBlockingTest causes
+        // an exception
+        // E/TestRunner: java.lang.IllegalStateException: This job has not completed yet
+        runBlocking {
+            repository.deleteAllTasks()
+        }
+    }
+
+    @After
+    fun reset() {
+        ServiceLocator.resetRepository()
+    }
+
+
+    @Test
+    fun editTask() {
+
+        runBlocking {
+            repository.saveTask(Task("TITLE1", "DESCRIPTION"))
+        }
+
+        // start up Tasks screen
+        val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
+
+        // Click on the task on the list and verify that all the data is correct
+        onView(withText("TITLE1")).perform(click())
+        onView(withId(R.id.task_detail_title_text)).check(matches(withText("TITLE1")))
+        onView(withId(R.id.task_detail_description_text)).check(matches(withText("DESCRIPTION")))
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(not(isChecked())))
+
+        // Click on the edit button, edit, and save
+        onView(withId(R.id.edit_task_fab)).perform(click())
+        onView(withId(R.id.add_task_title_edit_text)).perform(replaceText("NEW TITLE"))
+        onView(withId(R.id.add_task_description_edit_text)).perform(replaceText("NEW DESCRIPTION"))
+        onView(withId(R.id.save_task_fab)).perform(click())
+
+        // Verify task is displayed on screen in the task list.
+        onView(withText("NEW TITLE")).check(matches(isDisplayed()))
+        // Verify previous task is not displayed
+        onView(withText("TITLE1")).check(doesNotExist())
+        // Make sure the activity is closed before resetting the db:
+        activityScenario.close()
+    }
+}
